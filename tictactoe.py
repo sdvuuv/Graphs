30f522e4623958c1962099c117de6207143ec238
sdvuuv <sedovann05.irk@gmail.com>
2024-12-03 15:57:17 +0800

    alphabeta and ant fixed 2

--- tictactoe.py
+++ tictactoe.py
@@ -9,9 +9,11 @@ board = np.full((BOARD_SIZE, BOARD_SIZE), '.', dtype=str)
 
 def print_board():
     """Печатает игровое поле."""
-    for row in board:
-        print(" ".join(row))
-    print()
+    print("0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19")
+    for i in range(BOARD_SIZE):
+        for row in board:
+            print(i + " " " ".join(row))
+        print()
 
 def check_win(player):
     """Проверяет, выиграл ли игрок."""
